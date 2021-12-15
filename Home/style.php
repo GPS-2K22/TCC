0e8333d805f480d8a0ee8c754dfece9a62b0400a
Eegle380 <eegle380gamer@gmail.com>
2021-12-15 11:20:30 -0300

    add style php

---
+++ Home/style.php
@@ -0,0 +1,34 @@
+<style>
+table{
+    width:50%;
+    border:1px solid #ffffff;
+    text-align:center
+}
+th, td{
+    border:0.5px solid #ffffff;
+}
+body{
+    background-color:#010101;
+    color: #f1f1f1;
+    font-family: "arial";
+    
+    
+    
+}
+input:focus{
+    box-shadow: 0 0 0 0;
+    border: 0 none;
+    outline: 0;
+    
+            }
+            
+input{
+    
+    box-shadow: 0 0 0 0;
+    border: 0 none;
+    outline: 0;
+    margin-bottom: 1px;
+    
+    
+}		
+</style>
