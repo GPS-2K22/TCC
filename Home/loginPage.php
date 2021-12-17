9e9786f8aac18696de2564afc471e1c0b2c5f3da
Eegle380 <eegle380gamer@gmail.com>
2021-12-17 13:50:55 -0300

    Estilizada com css com base em template

--- Home/loginPage.php
+++ Home/loginPage.php
@@ -1,3 +1,5 @@
+
+<?php session_start();  //inicia a sessao ?>
 <?php include 'style.php'; ?>
 <html>
 <head>
@@ -5,7 +7,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -18,84 +21,139 @@
 
 
 	</script>
-	<?php session_start();  //inicia a sessao ?>
+	
+
 </head>
 <body>
+<div class="container">
+        <div class="content first-content">
+            <div class="first-column">
+                <h2 class="title title-primary">Bem-vindo de volta!</h2>
+                <p class="description description-primary">Se mantenha conectado conosco,</p>
+                <p class="description description-primary">caso possua uma conta clique no botão abaixo</p>
+                <button id="signin" class="btn btn-primary">Log In</button>
+            </div>    
+            <div class="second-column">
+                <h2 class="title title-second">Registrar Agora!</h2>
+               
+                <p class="description description-second">Insira as informações abaixo:</p>
+                <form class="form" name="f" id="form" method="post" action="crudFromLogIn.php">
+                    <label class="label-input" for="">
+                        <i class="far fa-user icon-modify"></i>
+                        <input type="text"   name="name" placeholder="Nome">
+                    </label>
+                    
+                    <label class="label-input" for="">
+                        <i class="far fa-envelope icon-modify"></i>
+                        <input type="email" name="email" placeholder="Email">
+                    </label>
+                    
+                    <label class="label-input" for="">
+                        <i class="fas fa-lock icon-modify"></i>
+                        <input type="password"name="password" placeholder="Senha">
+                    </label>
+                    
+                    <input type="text" name="Act" id="Act" style="display:none"></input><br>
 
-	<form name="f" id="form" method="post" action="crudFromLogIn.php">
-   
-		<input type="text" name="name" value="" placeholder="Nome" class="inpuTxt"></input><br>
-		<input type="text" name="email" value="" placeholder="Email" class="inpuTxt" ></input><br>
-		<input type="text" name="password" value="" placeholder="Senha" class="inpuTxt"></input><br>
-		<input type="text" name="Act" id="Act" style="display:none"></input><br>
+                    <button class="btn btn-second"  onclick="submitForm('c');">Criar!</button>        
+                </form>
+            </div><!-- second column -->
+        </div><!-- first content -->
+        <div class="content second-content">
+            <div class="first-column">
+                <h2 class="title title-primary">Ei!</h2>
+                <p class="description description-primary">Se você não possui conta que tal criar uma? </p>
+                <p class="description description-primary">clique no botão abaixo para começar!</p>
+                <button id="signup" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="second-column">
+                <h2 class="title title-second">entre em sua conta</h2>
+               
+                <p class="description description-second">use seu email e senha de cadastro nos espaços abaixo:</p>
+                <form class="form" name="f" id="forml" method="post" action="crudFromLogIn.php"">
+                
+                    <label class="label-input" for="">
+                        <i class="far fa-envelope icon-modify"></i>
+                        <input type="email"  name="emailLog" value="" placeholder="Email">
+                    </label>
+                
+                    <label class="label-input" for="">
+                        <i class="fas fa-lock icon-modify"></i>
+                        <input type="password" placeholder="Senha"  name="passwordLog" value="" >
+                    </label>
+					
+					
+					<input type="text" name="Act" id="ActL" style="display:none"></input><br>
+                    <a class="password" href="#">Esqueceu a senha?</a>
 
 
+                    <button  class="btn btn-second"   onclick="submitFormLog('l');">Logar!</button>
 
-		<input type="button" value="Create" onclick="submitForm('c');" class="buttonAss" id="btnC" ></input>
-		<input type="button" value="Restore" onclick="submitForm('r');"  class="buttonAss" id="btnR"></input>
-		<input type="button" value="Update" onclick="submitForm('u');"  class="buttonAss" id="btnU"></input>
-		<input type="button" value="Delete" onclick="submitForm('d');"   class="buttonAss"id="btnD"></input>
-	</form>
+					<?php
+if(isset($_SESSION["aviso"])){ //Se a variavel aviso foi criada na pagina anterior...
+    echo("<p class='description description-second'>".$_SESSION["aviso"]."</p>"); //imprime o aviso e...
+    unset($_SESSION["aviso"]); //exclui o aviso da memoria
+} 
+?>
+                </form>
+            </div><!-- second column -->
+        </div><!-- second-content -->
+    </div>
+<script>
 
 
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
+
+var body = document.querySelector("body");
 
 
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
 
-   
-    <form name="f" id="forml" method="post" action="crudFromLogIn.php">
-    <h1 color="white">Log In</h1>
-		<input type="text" name="emailLog" value="" placeholder="Email" class="inpuTxtLog" ></input><br>
-		<input type="text" name="passwordLog" value="" placeholder="Senha" class="inpuTxtLog"></input><br>
-		<input type="text" name="Act" id="ActL" style="display:none"></input><br>
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+})
 
-        <input type="button" value="Log-in" onclick="submitFormLog('l');" class="buttonAss" id="btnL" ></input>
+</script>
 
-	</form>
 
 <script>
-	function submitFormLog(paramAct){ //submete o formulario mas passando...
-		document.getElementById('ActL').value = paramAct; //o valor do campo de texto escondido para o valor do botao clicado para selecionar a acao e...
-		document.getElementById('forml').submit(); //efetua a submissao do formulario
-	}
-    
+function submitFormLog(paramAct){ //submete o formulario mas passando...
+document.getElementById('ActL').value = paramAct; //o valor do campo de texto escondido para o valor do botao clicado para selecionar a acao e...
+document.getElementById('forml').submit(); //efetua a submissao do formulario
+}
 
 
-	</script>
 
+</script>
 
-	<br><br>
-	<?php
-		if(isset($_SESSION["aviso"])){ //Se a variavel aviso foi criada na pagina anterior...
-			echo($_SESSION["aviso"]); //imprime o aviso e...
-			unset($_SESSION["aviso"]); //exclui o aviso da memoria
-		} 
-	?>
-	<hr /><br>
-   <?php
-  
-		$con = new mysqli("127.0.0.1:3306", "root", "", "users"); //executa a conexao com o banco
-		$sql = "select * from usersreg"; // seleciona todos os dados ta tabela produtos
-		$res = $con->query($sql); //executa a consulta SQL
-		if(mysqli_num_rows($res) > 0){ //checa se foram encontrados resultados
-			echo("<div id='tbEx'>");
-			echo("<table>"); //daqui pra baixo estamos montando a tabela
-			echo("<tr><th>Id</th><th>Nome</th><th>Email</th><th>Senha</th></tr>"); //cabecalho da tabela
-			while($campo = $res->fetch_assoc()){ //para cada linha de resultado recuperada da consulta monta uma linha em <table>
-				echo("<tr>");
-				echo("<td>".$campo["idUser"]."</td>"); //seleciona o valor no campo 'idprod' para a execucao do laco atual do while
-				echo("<td>".$campo["name"]."</td>"); //seleciona o valor no campo 'nome' para a execucao do laco atual do while
-				echo("<td>".$campo["email"]."</td>");
-				echo("<td>".$campo["pwd"]."</td>"); //seleciona o valor no campo 'imagem' para a execucao do laco atual do while
-				echo("</tr>");
-			}
-			echo("</table>"); //finaliza a tabela
-			echo("</div>");
-		}
-		else{ //nenhum dado na tabela
-			echo "nenhum dado inserido por enquanto";
-		}
-	
-	?>
+<?php
+
+$con = new mysqli("127.0.0.1:3306", "root", "", "users"); //executa a conexao com o banco
+$sql = "select * from usersreg"; // seleciona todos os dados ta tabela produtos
+$res = $con->query($sql); //executa a consulta SQL
+if(mysqli_num_rows($res) > 0){ //checa se foram encontrados resultados
+    echo("<div id='tbEx'>");
+    echo("<table>"); //daqui pra baixo estamos montando a tabela
+    echo("<tr><th>Id</th><th>Nome</th><th>Email</th><th>Senha</th></tr>"); //cabecalho da tabela
+    while($campo = $res->fetch_assoc()){ //para cada linha de resultado recuperada da consulta monta uma linha em <table>
+        echo("<tr>");
+        echo("<td>".$campo["idUser"]."</td>"); //seleciona o valor no campo 'idprod' para a execucao do laco atual do while
+        echo("<td>".$campo["name"]."</td>"); //seleciona o valor no campo 'nome' para a execucao do laco atual do while
+        echo("<td>".$campo["email"]."</td>");
+        echo("<td>".$campo["pwd"]."</td>"); //seleciona o valor no campo 'imagem' para a execucao do laco atual do while
+        echo("</tr>");
+    }
+    echo("</table>"); //finaliza a tabela
+    echo("</div>");
+}
+else{ //nenhum dado na tabela
+    echo "nenhum dado inserido por enquanto";
+}
+
+?>
 	
 <body>
 </html>
--- Home/style.php
+++ Home/style.php
@@ -1,9 +1,549 @@
 
 <style>
 /*Estilos Para Usar no  Programa Todo*/
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap');
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    font-family: 'Open Sans', sans-serif;
+    background-color: #282b30;
+}
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #282b30;
+}
+.content {
+    background-color: #40444b;
+    border-radius: 15px;
+    width: 960px;
+    height: 50%;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+}
+.content::before {
+    content: "";
+    position: absolute;
+    background-color: #589baf;
+    width: 40%;
+    height: 100%;
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+
+
+    left: 0;
+}
+.title {
+    font-size: 28px;
+    font-weight: bold;
+    text-transform: capitalize;
+}
+.title-primary {
+    color: #fff;
+}
+.title-second {
+    color: #589baf;
+}
+.description {
+    font-size: 14px;
+    font-weight: 300;
+    line-height: 30px;
+}
+.description-primary {
+    color: #fff;
+}
+.description-second {
+    color: #7f8c8d;
+}
+.btn {
+    border-radius: 15px;
+    text-transform: uppercase;
+    color: #fff;
+    font-size: 10px;
+    padding: 10px 50px;
+    cursor: pointer;
+    font-weight: bold;
+    width: 150px;
+    align-self: center;
+    border: none;
+    margin-top: 1rem;
+}
+.btn-primary {
+    background-color: transparent;
+    border: 1px solid #fff;
+    transition: background-color .5s;
+}
+.btn-primary:hover {
+    background-color: #fff;
+    color: #58af9b;
+}
+.btn-second {
+    background-color: #589baf;
+    border: 1px solid #589baf;
+    transition: background-color .5s;
+}
+.btn-second:hover {
+    background-color: #fff;
+    border: 1px solid #589baf;
+    color: #589baf;
+}
+.first-content {
+    display: flex;
+}
+.first-content .second-column {
+    z-index: 11;
+}
+.first-column {
+    text-align: center;
+    width: 40%;
+    z-index: 10;
+}
+.second-column {
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.social-media {
+    margin: 1rem 0;
+}
+.link-social-media:not(:first-child){
+    margin-left: 10px;
+}
+.link-social-media .item-social-media {
+    transition: background-color .5s;
+}
+.link-social-media:hover .item-social-media{
+    background-color: #58af9b;
+    color: #fff;
+    border-color: #58af9b;
+}
+.list-social-media {
+    display: flex;
+    list-style-type: none;
+}
+.item-social-media {
+    border: 1px solid #bdc3c7;
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+    line-height: 35px;
+    text-align: center;
+    color: #95a5a6;
+}
+.form {
+    display: flex;
+    flex-direction: column;
+    width: 55%;
+}
+.form input {
+    height: 45px;
+    width: 100%;
+    border: none;
+    user-select:none;
+    background-color: #ecf0f1;
+}
+.form input:focus{
+    box-shadow: 0 0 0 0;
+    border: 0 none;
+    outline: 0;
+}
+input:-webkit-autofill 
+{    
+    -webkit-box-shadow: 0 0 0px 1000px #ecf0f1 inset !important;
+    -webkit-text-fill-color: #000 !important;
+}
+.label-input {
+    background-color: #ecf0f1;
+    display: flex;
+    align-items: center;
+    margin: 8px;
+}
+.icon-modify {
+    color: #7f8c8d;
+    padding: 0 5px;
+}
+
+/* second content*/
+
+.second-content {
+    position: absolute;
+    display: flex;
+}
+.second-content .first-column {
+    order: 2;
+    z-index: -1;
+}
+.second-content .second-column {
+    order: 1;
+    z-index: -1;
+}
+.password {
+    color: #7f8c8d;
+    font-size: 14px;
+    margin: 15px 0;
+   
+    text-align: center;
+}
+.password::first-letter {
+    text-transform: capitalize;
+}
+
+
+
+.sign-in-js .first-content .first-column {
+    z-index: -1;
+}
+
+.sign-in-js .second-content .second-column {
+    z-index: 11;
+}
+.sign-in-js .second-content .first-column {
+    z-index: 13;
+}
+
+.sign-in-js .content::before {
+    left: 60%;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+    animation: slidein 0.15s; /*MODIFIQUEI DE 3s PARA 1.3s*/
+
+    z-index: 12;
+}
+
+.sign-up-js .content::before {
+    animation: slideout 0.3s; /*MODIFIQUEI DE 3s PARA 1.3s*/
+
+    z-index: 12;
+}
+
+.sign-up-js .second-content .first-column,
+.sign-up-js .second-content .second-column {
+    z-index: -1;
+}
+
+.sign-up-js .first-content .second-column {
+    z-index: 11;
+}
+
+.sign-up-js .first-content .first-column {
+    z-index: 13;    
+}
+
+
+/* DESLOCAMENTO CONTEÚDO ATRÁS DO CONTENT:BEFORE*/
+.sign-in-js .first-content .second-column {
+
+    z-index: -1;
+    position: relative;
+    animation: deslocamentoEsq 0.3s; /*MODIFIQUEI DE 3s PARA 1.3s*/
+}
+
+.sign-up-js .second-content .second-column {
+    position: relative;
+    z-index: -1;
+    animation: deslocamentoDir 0.3s; /*MODIFIQUEI DE 3s PARA 1.3s*/
+}
+
+/*ANIMAÇÃOO CSS PARA O CONTEÚDO*/
+
+@keyframes deslocamentoEsq {
+
+    from {
+        left: 0;
+        opacity: 1;
+        z-index: 12;
+    }
+
+    25% {
+        left: -80px;
+        opacity: .5;
+        /* z-index: 12; NÃO HÁ NECESSIDADE */
+    }
+
+    50% {
+        left: -100px;
+        opacity: .2;
+        /* z-index: 12; NÃO HÁ NECESSIDADE */
+    }
+
+    to {
+        left: -110px;
+        opacity: 0;
+        z-index: -1;
+    }
+}
+
+
+@keyframes deslocamentoDir {
+
+    from {
+        left: 0;
+        z-index: 12;
+    }
+
+    25% {
+        left: 80px;
+        /* z-index: 12;  NÃO HÁ NECESSIDADE*/
+    }
+
+    50% {
+        left: 100px;
+        /* z-index: 12; NÃO HÁ NECESSIDADE*/
+        /* background-color: yellow;  Exemplo que dei no vídeo*/
+    }
+
+    to {
+        left: 110px;
+        z-index: -1;
+    }
+}
+
+
+/*ANIMAÇÃO CSS*/
+
+@keyframes slidein {
+
+    from {
+        left: 0;
+        width: 40%;
+    }
+
+    25% {
+        left: 5%;
+        width: 50%;
+    }
+
+    50% {
+        left: 25%;
+        width: 60%;
+    }
+
+    75% {
+        left: 45%;
+        width: 50%;
+    }
+
+    to {
+        left: 60%;
+        width: 40%;
+    }
+}
+
+@keyframes slideout {
+
+    from {
+        left: 60%;
+        width: 40%;
+    }
+
+    25% {
+        left: 45%;
+        width: 50%;
+    }
+
+    50% {
+        left: 25%;
+        width: 60%;
+    }
+
+    75% {
+        left: 5%;
+        width: 50%;
+    }
+
+    to {
+        left: 0;
+        width: 40%;
+    }
+}
+
+/*VERSÃO MOBILE*/
+@media screen and (max-width: 1040px) {
+    .content {
+        width: 100%;
+        height: 100%;
+    }
+
+    .content::before {
+        width: 100%;
+        height: 40%;
+        top: 0;
+        border-radius: 0;
+    }
+    .first-content, .second-content {
+        flex-direction: column;
+        justify-content: space-around;
+    }
+
+    .first-column, .second-column {
+        width: 100%;
+    }
+    
+    .sign-in-js .content::before {
+        top: 60%;
+        left: 0;
+        border-radius: 0;
+
+    }
+
+    .form {
+        width: 90%;
+    }
+    
+    /* ANIMAÇÃO MOBILE CSS*/
+
+    @keyframes deslocamentoEsq {
+
+        from {
+            top: 0;
+            opacity: 1;
+            z-index: 12;
+        }
+    
+        25% {
+            top: -80px;
+            opacity: .5;
+            /* z-index: 12; NÃO HÁ NECESSIDADE */
+        }
+    
+        50% {
+            top: -100px;
+            opacity: .2;
+            /* z-index: 12; NÃO HÁ NECESSIDADE */
+        }
+    
+        to {
+            top: -110px;
+            opacity: 0;
+            z-index: -1;
+        }
+    }
+    
+    
+    @keyframes deslocamentoDir {
+    
+        from {
+            top: 0;
+            z-index: 12;
+        }
+    
+        25% {
+            top: 80px;
+            /* z-index: 12;  NÃO HÁ NECESSIDADE*/
+        }
+    
+        50% {
+            top: 100px;
+            /* z-index: 12; NÃO HÁ NECESSIDADE*/
+            /* background-color: yellow;  Exemplo que dei no vídeo*/
+        }
+    
+        to {
+            top: 110px;
+            z-index: -1;
+        }
+    }
+    
+    
+    
+    @keyframes slidein {
+    
+        from {
+            top: 0;
+            height: 40%;
+        }
+    
+        25% {
+            top: 5%;
+            height: 50%;
+        }
+    
+        50% {
+            top: 25%;
+            height: 60%;
+        }
+    
+        75% {
+            top: 45%;
+            height: 50%;
+        }
+    
+        to {
+            top: 60%;
+            height: 40%;
+        }
+    }
+    
+    @keyframes slideout {
+    
+        from {
+            top: 60%;
+            height: 40%;
+        }
+    
+        25% {
+            top: 45%;
+            height: 50%;
+        }
+    
+        50% {
+            top: 25%;
+            height: 60%;
+        }
+    
+        75% {
+            top: 5%;
+            height: 50%;
+        }
+    
+        to {
+            top: 0;
+            height: 40%;
+        }
+    }
+   
+}
+
+@media screen and (max-width: 740px) {
+    .form {
+        width: 50%;
+    }
+}
+
+@media screen and (max-width: 425px) {
+    .form {
+        width: 100%;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #tbEx{
-    position:fixed;
+    position:relative;
     left:25%;
+    z-index:40;
+    top: 10%
     width: 100%;
    
 
@@ -16,8 +556,9 @@ h1{
 
 }
 table{
-  
+    background-color:#40444b;
     width:50%;
+    color:#fff;
     border:1px solid #ffffff;
     text-align:center
     
@@ -27,214 +568,8 @@ th, td{
     
     user-select: none;
 }
-body{
-    background-color:#010101;
-    color: #f1f1f1;
-    font-family: "arial";
-    
-    
-    
-}
-
-
-/****************************** */
-
-
-
-.inpuTxt:focus{
-    box-shadow: 0 0 0 0;
-    border: 0 none;
-    outline: 0;
-    
-            }
-            
-.inpuTxt{
-    margin-left: 40%;
-   
-    margin-bottom: 1.5%;
-    border-radius: 5px;
-    color: #cbcdc9;
-    background-color: #40444b;
-    
-
-
-    width: 20%;
-    height: 4%;
-    user-select: none;
-
-    font-size: 15px;
-    border: none;
-
-    
-    
-}		
-.inpuTxt::placeholder{
-    color: #6d7078;
- 
-
-}
-
-/*********************************************************** */
 
 
 
 
-
-.inpuTxtLog:focus{
-    box-shadow: 0 0 0 0;
-    border: 0 none;
-    outline: 0;
-    
-            }
-            
-.inpuTxtLog{
-    margin-left: 40%;
-   
-    margin-bottom: 1.5%;
-    border-radius: 5px;
-    color: #cbcdc9;
-    background-color: #40444b;
-    
-
-
-    width: 20%;
-    height: 4%;
-    user-select: none;
-
-    font-size: 15px;
-    border: none;
-
-    
-    
-}		
-.inpuTxtLog::placeholder{
-    color: #6d7078;
- 
-
-}
-
-
-
-
-
-
-
-
-/************************************************************* */
-
-.buttonAss{
-  position: fixed;
-  diplay: inline;
-
-  background: transparent;
-  top:19%;
-  border: solid 2px;
-
-  cursor: pointer;
-  height: 7%;
-  font-size: 120%;
-  user-select: none;
-  width: 10%;
-  border-radius: 5%;
-  color: #ffffff;
- 
-  
-
-}
-.buttonAss:disabled{
-   background-color: rgb(195, 231, 210);
-  
-}
-
-
-.buttonAss:hover{
-  transition: 0.2s;
-  /*background-color: rgb(53, 245, 133);*/
- 
-  height: 8%;
-
-}
-
-/******************************* */
-#btnC{
-    margin-left: 28%;
-    color: #3acf78;
-    border-color:#2c9c5b;
-
-}
-#btnC:hover{
-    border-color: #3acf78;
-    color: #2c9c5b;
-    background-color: #3acf78;
- 
-    
-}
-
-/**********************************/
-#btnR{
-    margin-left: 40.5%;
-    border-color: #959c2c;
-    color: #e8f800;
-
-    
-}
-
-#btnR:hover{
-    border-color: #e8f800;
-    color: #959c2c;
-    background-color: #e8f800;
- 
-    
-}
-
-/*****************************/
-#btnU{
-    border-color: #6161d1;
-    color: #85bafc;
-    margin-left: 52.5%;
-    
-}
-#btnU:hover{
-    border-color: #85bafc;
-    color: #6161d1;
-    background-color: #85bafc;
- 
-    
-}
-
-/****************************************/
-#btnD{
-    border-color: #af2525;
-    color: #ec1414;
-    margin-left: 64.5%;
-    
-}
-
-#btnD:hover{
-    border-color: #ec1414;
-    color: #af2525;;
-    background-color: #ec1414;
- 
-    
-}
-/*************************************/
-
-
-/******************************* */
-#btnL{
-    top:50%;
-    margin-left: 28%;
-    color: #3acf78;
-    border-color:#2c9c5b;
-
-}
-#btnL:hover{
-    border-color: #3acf78;
-    color: #2c9c5b;
-    background-color: #3acf78;
- 
-    
-}
-
-/**********************************/
 </style>
