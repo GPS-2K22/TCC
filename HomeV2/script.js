ce752dca8680f4a2938fc3d38ef0dfc9779cea5c
GEnacheC <guilherme.enache@gmail.com>
2022-03-01 13:42:37 -0300

    Adicionando a função explanation(responsavel por explicar um código traduzido

--- HomeV2/script.js
+++ HomeV2/script.js
@@ -1,26 +1,51 @@
 var pi = ["PRINT"];
 var js = ["CONSOLE.LOG"];
 var php = ["ECHO"]
+
+function whatIs(what){
+    if(what == 'command'){
+        return "A função print() é uma das funções mais importantes e usadas na linguagem Python. Sua função é, basicamente, exibir mensagens na tela ou enviá-las para outro dispositivo, como imprimir dentro de arquivos de texto"
+    }
+    if(what == 'string'){
+        return 'String é uma sequência de caracteres, é uma forma de designar uma coleção de dados heterogêneos com um significado especial e muito usado em códigos. Então o valor que compõe um texto qualquer tem esse nome, e muitas vezes o tipo do objeto que tem essa informação é também chamada de string ou String em algumas linguagens.'
+    }
+    if(what == ''){
+        return ''
+    }
+}
+
 function translateCommand(){
     let errorMessage = document.getElementById('errorMessage')
-    let command = document.getElementById("command").value.toUpperCase();
+    let command = document.getElementById("command").value;
         let translate = document.getElementById("commandTranslated");
         let strings = command.split("'")[1];
-        let AfeterBrackets = command.split("'")[2];
-        let Commands = command.split("(")[0];
-        let exp = document.getElementById('explanation');
-    
-    for (let i = 0; i < command.length; i++) {
-      
-        if(command[i]== "(" && command[command.length-2] == ")"){
-          
-            
-        }
-        // more statements
-     }
+        let AfterBrackets = command.split("'")[2];
+        let Commands = command.split("(")[0].toUpperCase();
+        let exp = document.querySelector('.explanation');
+
     if(js.includes(Commands)  || php.includes(Commands)){   
-        translate.value =  (pi[0] + "('" + strings  + "'"+ AfeterBrackets).toLowerCase();
+        let translatedCommands = pi[0].toLowerCase() + "('" + strings  + "'"+ AfterBrackets;
+        translate.value =  translatedCommands;
         exp.style.display  = "block";
+
+        for (let i = 1; i <= 3; i++) {
+            let order = document.querySelector('.order--' + i)
+            //console.log(order)
+            if(i == 1){
+                order.textContent = pi[0].toLowerCase();
+                order.style.color = "#61afef";
+            } 
+            if(i == 2){
+                order.textContent = "('" + strings + "'" + AfterBrackets;
+                order.style.color = "#98c379";
+            }   
+            if(i == 3){
+                //order.textContent = whatIs('command');
+                order.style.color = "#61afef";
+                order.style.fontSize = "40%";
+                order.style.marginTop = "10px";
+            }
+         }
       
     }
     else{
@@ -35,4 +60,24 @@ function closeError(){
 }
 
 
+//A fazer - explanation, mais comandos e organização
 
+
+var order = document.querySelector(".order--3")
+var item = document.querySelector(".order--1");
+var item2 = document.querySelector(".order--2");
+item.addEventListener("mouseover", changeToCommand, false);
+item2.addEventListener("mouseover", changeToString, false);
+
+
+function changeToCommand()
+{  
+    order.textContent = whatIs('command');
+    order.style.color = "#61afef";
+}
+
+function changeToString()
+{  
+    order.textContent = whatIs('string');
+    order.style.color = "#98c379";
+}
