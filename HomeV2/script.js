66a9e21d251f5e630f9b71fd944c64211cf0d898
GEnacheC <guilherme.enache@gmail.com>
2022-03-02 22:03:15 -0300

    adicionando boostrap  e +

--- HomeV2/script.js
+++ HomeV2/script.js
@@ -2,6 +2,8 @@ var pi = ["PRINT"];
 var js = ["CONSOLE.LOG"];
 var php = ["ECHO"]
 
+
+
 function whatIs(what){
     if(what == 'command'){
         return "A função print() é uma das funções mais importantes e usadas na linguagem Python. Sua função é, basicamente, exibir mensagens na tela ou enviá-las para outro dispositivo, como imprimir dentro de arquivos de texto"
@@ -15,6 +17,7 @@ function whatIs(what){
 }
 
 function translateCommand(){
+    let LangSelect = document.querySelector('input[name="lang"]:checked').value;
     let errorMessage = document.getElementById('errorMessage')
     let command = document.getElementById("command").value;
         let translate = document.getElementById("commandTranslated");
@@ -22,37 +25,116 @@ function translateCommand(){
         let AfterBrackets = command.split("'")[2];
         let Commands = command.split("(")[0].toUpperCase();
         let exp = document.querySelector('.explanation');
+    if(LangSelect  == "Python"){
+        if(js.includes(Commands) || php.includes(Commands)){   
+            let translatedCommands = pi[0].toLowerCase() + "('" + strings  + "'"+ AfterBrackets;
+            translate.value =  translatedCommands;
+            exp.style.display  = "block";
 
-    if(js.includes(Commands)  || php.includes(Commands)){   
-        let translatedCommands = pi[0].toLowerCase() + "('" + strings  + "'"+ AfterBrackets;
-        translate.value =  translatedCommands;
-        exp.style.display  = "block";
-
-        for (let i = 1; i <= 3; i++) {
-            let order = document.querySelector('.order--' + i)
-            //console.log(order)
-            if(i == 1){
-                order.textContent = pi[0].toLowerCase();
-                order.style.color = "#61afef";
-            } 
-            if(i == 2){
-                order.textContent = "('" + strings + "'" + AfterBrackets;
-                order.style.color = "#98c379";
-            }   
-            if(i == 3){
-                //order.textContent = whatIs('command');
-                order.style.color = "#61afef";
-                order.style.fontSize = "40%";
-                order.style.marginTop = "10px";
+            for (let i = 1; i <= 3; i++) {
+                let order = document.querySelector('.order--' + i)
+                //console.log(order)
+                if(i == 1){
+                    order.textContent = pi[0].toLowerCase();
+                    order.style.color = "#61afef";
+                } 
+                if(i == 2){
+                    order.textContent = "('" + strings + "'" + AfterBrackets;
+                    order.style.color = "#98c379";
+                }   
+                if(i == 3){
+                    //order.textContent = whatIs('command');
+                    order.style.color = "#61afef";
+                    order.style.fontSize = "10%";
+                    order.style.marginTop = "10px";
+                }
             }
-         }
       
+        }
+        else{
+            translate.value = "";
+            exp.style.display  = "none";
+            errorMessage.style.display = "block";
+        }
+    }
+
+
+
+    else if(LangSelect  == "JS"){
+        if(pi.includes(Commands)  ||  php.includes(Commands)){   
+            let translatedCommands = js[0].toLowerCase() + "('" + strings  + "'"+ AfterBrackets;
+            translate.value =  translatedCommands;
+            exp.style.display  = "block";
+
+            for (let i = 1; i <= 3; i++) {
+                let order = document.querySelector('.order--' + i)
+                //console.log(order)
+                if(i == 1){
+                    order.textContent = js[0].toLowerCase();
+                    order.style.color = "#61afef";
+                } 
+                if(i == 2){
+                    order.textContent = "('" + strings + "'" + AfterBrackets;
+                    order.style.color = "#98c379";
+                }   
+                if(i == 3){
+                    //order.textContent = whatIs('command');
+                    order.style.color = "#61afef";
+                    order.style.fontSize = "40%";
+                    order.style.marginTop = "10px";
+                }
+            }
+      
+        }
+        else{
+            translate.value = "";
+            exp.style.display  = "none";
+            errorMessage.style.display = "block";
+        }
+    } 
+
+    else if(LangSelect  == "PHP"){
+        if(pi.includes(Commands)  ||  js.includes(Commands)){   
+            let translatedCommands = php[0].toLowerCase() + "('" + strings  + "'"+ AfterBrackets;
+            translate.value =  translatedCommands;
+            exp.style.display  = "block";
+
+            for (let i = 1; i <= 3; i++) {
+                let order = document.querySelector('.order--' + i)
+                //console.log(order)
+                if(i == 1){
+                    order.textContent = php[0].toLowerCase();
+                    order.style.color = "#61afef";
+                } 
+                if(i == 2){
+                    order.textContent = "('" + strings + "'" + AfterBrackets;
+                    order.style.color = "#98c379";
+                }   
+                if(i == 3){
+                    //order.textContent = whatIs('command');
+                    order.style.color = "#61afef";
+                    order.style.fontSize = "40%";
+                    order.style.marginTop = "10px";
+                }
+            }
+      
+        }
+        else{
+            translate.value = "";
+            exp.style.display  = "none";
+            errorMessage.style.display = "block";
+        }
     }
     else{
         translate.value = "";
         exp.style.display  = "none";
         errorMessage.style.display = "block";
     }
+
+
+
+   
+    
 }
 function closeError(){
     let errorMessage = document.getElementById('errorMessage')
@@ -60,14 +142,16 @@ function closeError(){
 }
 
 
-//A fazer - explanation, mais comandos e organização
+//Feito - explanation, mais comandos e organização
+// A fazer - relação entre comandos(talvez banco de dados), escolher linguagem
 
 
+// Hover 
 var order = document.querySelector(".order--3")
-var item = document.querySelector(".order--1");
-var item2 = document.querySelector(".order--2");
-item.addEventListener("mouseover", changeToCommand, false);
-item2.addEventListener("mouseover", changeToString, false);
+var commandHover = document.querySelector(".order--1");
+var stringHover = document.querySelector(".order--2");
+commandHover.addEventListener("mouseover", changeToCommand, false);
+stringHover.addEventListener("mouseover", changeToString, false);
 
 
 function changeToCommand()
