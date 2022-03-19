ef8fa256cfde16c2dd559dfca6f89e25596f8ae6
GEnacheC <guilherme.enache@gmail.com>
2022-03-19 15:10:08 -0300

    Comentando

--- HomeV2/script.js
+++ HomeV2/script.js
@@ -15,7 +15,7 @@ $(function(){
 });
 
 
-
+//func ident
 function whatIs(what){
     if(what == 'command'){
         return "A função print() é uma das funções mais importantes e usadas na linguagem Python. Sua função é, basicamente, exibir mensagens na tela ou enviá-las para outro dispositivo, como imprimir dentro de arquivos de texto"
