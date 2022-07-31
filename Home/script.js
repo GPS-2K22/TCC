b1f805c90de9fa84ae79482dd4ad64f5c7216b75
GEnacheC <guilherme.enache@gmail.com>
2022-07-31 18:35:41 -0300

    Atualizando Home

---
+++ Home/script.js
@@ -0,0 +1,368 @@
+const AutoCompleteValue =[
+    // Prints
+    {name : "print", syntax : 'Escrever', usage: "Escrever uma mensagem no console"},
+    {name : "print", syntax :'Escrever("mensagem")', usage: "Escrever uma mensagem no console"},
+    {name : "print", syntax :'Escrever("mensagem", variavel)', usage: "Escrever uma mensagem no console"},
+    // Conditionals
+    {name : "if", syntax : 'Se', usage: "Condicional"},
+    {name : "if", syntax :'Se(valor_1 == valor_2)', usage: "Condicional"},
+    {name : "if", syntax :'Se(valor_1 >= valor_2)', usage: "Condicional"},
+    {name : "if", syntax :'Se(valor_1 <= valor_2)', usage: "Condicional"},
+    {name : "if", syntax :'Se(valor_1 > valor_2)', usage: "Condicional"},
+    {name : "if", syntax :'Se(valor_1 < valor_2)', usage: "Condicional"}
+];
+const simbols =["==",">=","<=",">","<"];
+const JAVA_api = [
+    {name:"public_void", syntax: "public static void main(String args[])", usage:""},
+    {name:"print", syntax: "System.out.println", usage:""},
+    {name:"start_class", syntax: "class", usage:""},
+    {name:"str", syntax: "String", usage:""},
+    {name:"if", syntax: "if", usage:""},
+]
+const Python_api = [
+    {name: "print", syntax: "print",usage:""},
+    {name: "if", syntax: "if",usage:""}
+]
+const JS_api=[
+    {name:"print",syntax:"console.log",usage:""},
+    {name:"var",syntax:"var",usage:""},
+    {name:"if",syntax:"if",usage:""},
+]
+class Translate {
+    // constructor (userCommand){
+    //     this.command = userCommand
+     
+    // }
+    command(com,lang){
+        if(lang == "JAVA"){
+            return JAVA_api.find(element => element.name == com).syntax
+        }
+        if(lang == "PYTHON"){
+            return Python_api.find(element => element.name == com).syntax
+        }
+        if(lang == "JS"){
+            return JS_api.find(element => element.name == com).syntax
+        }
+        }
+    identyType(rawcommand){
+        function whatIs(par,per){
+            for(let i =0;  i< per.length;i++){
+                if(par == per[i].syntax){
+                      return per[i].name
+                }
+            }
+        }
+        
+        let command = whatIs(rawcommand.split("(")[0], AutoCompleteValue);
+        if(command == 'if'){
+            return "if"    
+        }
+        if(command == 'print'){
+            let string = rawcommand.split('"')[1];
+            let variable = rawcommand.split(',')[1];
+            if(string != undefined && variable != undefined){
+                return "print_var";
+            
+            }
+            else if(string != undefined && variable == undefined){
+                return "print_no_var"
+            }
+            else{
+                return "error";
+            }
+        }
+    }
+    identyCommand(command,lang){
+        function verifyConditional(par, per){
+            for(let i =0; i<= per.length;i++){
+                if(par.indexOf(per[i]) > -1)
+                      return per[i]
+            }
+        }
+        function hare(request){
+            let command = request.split("(")[0]
+            for(let i = 0;i <= AutoCompleteValue.length;i++){
+                if(command == AutoCompleteValue[i].syntax){
+                    return AutoCompleteValue[i].name;
+                }
+            }
+        }
+        if(lang == "JAVA"){ // -> Tortoise
+            if(this.identyType(command) == "print_var"){
+                let variable = command.split(',')[1].replace(')','');
+                let string = command.split('"')[1];
+                for(let i = 0; i <= JAVA_api.length; i++){
+                    if(JAVA_api[i].name == hare(command)){ 
+                        return [JAVA_api[i].syntax,variable,string]
+                    }    
+                }
+            }
+            if(this.identyType(command) == "print_no_var"){
+                let string = command.split('"')[1];
+                for(let i = 0; i <= JAVA_api.length; i++){
+                    if(JAVA_api[i].name == hare(command)){ 
+                        return [JAVA_api[i].syntax,string]
+                    }    
+                }
+            }
+            else if(this.identyType(command) == "if"){
+                let afterCommand = command.split("(")[1].replace(')', '')
+                let value_1 = afterCommand.split(verifyConditional(afterCommand,simbols))[0]
+                let value_2 = afterCommand.split(verifyConditional(afterCommand,simbols))[1]
+                let conditional = verifyConditional(afterCommand,simbols)
+                for(let i = 0; i <= JAVA_api.length; i++){
+                    if(JAVA_api[i].name == hare(command)){
+                        return [JAVA_api[i].syntax,value_1,value_2,conditional]
+                    }    
+                }
+            }
+        }
+        else if(lang == "PYTHON"){
+            if(this.identyType(command) == "print_var"){
+                let variable = command.split(',')[1].replace(')','');
+                let string = command.split('"')[1];
+                for(let i = 0; i <= Python_api.length; i++){
+                    if(Python_api[i].name == hare(command)){
+                        return [Python_api[i].syntax,variable,string]
+                    }    
+                }
+            }
+            if(this.identyType(command) == "print_no_var"){
+                let string = command.split('"')[1];
+                for(let i = 0; i <= Python_api.length; i++){
+                    if(Python_api[i].name == hare(command)){
+                        return [Python_api[i].syntax,string]
+                    }    
+                }
+            }
+            else if(this.identyType(command) == "if"){
+                let afterCommand = command.split("(")[1].replace(')', '')
+                let value_1 = afterCommand.split(verifyConditional(afterCommand,simbols))[0]
+                let value_2 = afterCommand.split(verifyConditional(afterCommand,simbols))[1]
+                let conditional = verifyConditional(afterCommand,simbols)
+                for(let i = 0; i <= Python_api.length; i++){
+                    if(Python_api[i].name == hare(command)){
+                        return [Python_api[i].syntax,value_1,value_2,conditional]
+                    }    
+                }
+            }
+        }
+        else if(lang == "JS"){
+            if(this.identyType(command) == "print_var"){
+                let variable = command.split(',')[1].replace(')','');
+                let string = command.split('"')[1];
+                for(let i = 0; i <= JS_api.length; i++){
+                    if(JS_api[i].name == hare(command)){
+                        return [JS_api[i].syntax,variable,string]
+                    }    
+                }
+            }
+            if(this.identyType(command) == "print_no_var"){
+                let string = command.split('"')[1];
+                for(let i = 0; i <= JS_api.length; i++){
+                    if(JS_api[i].name == hare(command)){
+                        return [JS_api[i].syntax,string]
+                    }    
+                }
+            }
+            else if(this.identyType(command) == "if"){
+                let afterCommand = command.split("(")[1].replace(')', '')
+                let value_1 = afterCommand.split(verifyConditional(afterCommand,simbols))[0]
+                let value_2 = afterCommand.split(verifyConditional(afterCommand,simbols))[1]
+                let conditional = verifyConditional(afterCommand,simbols)
+                for(let i = 0; i <= JS_api.length; i++){
+                    if(JS_api[i].name == hare(command)){
+                        return [JS_api[i].syntax,value_1,value_2,conditional]
+                    }    
+                }
+            }
+        }
+    }
+    structure(rawCommand,command, lang){
+        function count(num){
+            for(let i = 1; i <= num; i++){
+                let codeEnd = document.getElementById("end");
+                codeEnd.innerText += "}\n"
+            }
+        }
+        let codeStart = document.getElementById("start");
+        let codeMid = document.getElementById("mid");
+        let codeEnd = document.getElementById("end");
+        if(lang == "JAVA"){
+            codeStart.innerText = ``;
+            codeStart.innerText += `${this.command("start_class","JAVA")} Primeira_Classe{`;
+            codeStart.innerText += `\n${this.command("public_void","JAVA")}{`
+            if(codeEnd.innerHTML == ""){
+                count(2)
+            }
+            if(this.identyType(rawCommand) == "print_var"){
+                codeMid.innerText += `${this.command("str","JAVA")} first_var = ${command[1]}\n`
+                codeMid.innerText += `${command[0]}("${command[2]}", first_var)\n`
+                
+            }
+            if(this.identyType(rawCommand) == "print_no_var"){
+                codeMid.innerText += `${command[0]}("${command[1]}")\n`
+                
+            }
+            if(this.identyType(rawCommand) == "if"){
+                let val_1 = command[1]
+                let val_2 = command[2]
+                let cond = command[3]
+                let inp = document.getElementById("cond")
+                let btn = document.getElementById("condButton");
+                btn.style.display= "block"
+                inp.style.display= "block"
+                btn.onclick = function()
+                {
+                    translateCallBack(inp.value, "JAVA")
+
+                }
+                codeMid.innerText += `${command[0]}(${val_1} ${cond} ${val_2}){\n   `
+                count(1)
+            }
+           
+        }
+        if(lang == "PYTHON"){
+            if(this.identyType(rawCommand) == "print_var"){
+                codeMid.innerText += `first_var = ${command[1]}`
+                codeMid.innerText += `\n${command[0]}("${command[2]}", first_var)\n`
+                
+            }
+            if(this.identyType(rawCommand) == "print_no_var"){
+                codeMid.innerText += `${command[0]}("${command[1]}")\n`
+                
+            }
+            if(this.identyType(rawCommand) == "if"){
+                let val_1 = command[1]
+                let val_2 = command[2]
+                let cond = command[3]
+                let inp = document.getElementById("cond")
+                let btn = document.getElementById("condButton");
+                btn.style.display= "block"
+                inp.style.display= "block"
+                btn.onclick = function()
+                {
+                    translateCallBack(inp.value, "PYTHON")
+
+                }
+                codeMid.innerText += `${command[0]} ${val_1} ${cond} ${val_2}:\n   `
+            }
+        }
+        if(lang == "JS"){
+            if(this.identyType(rawCommand) == "print_var"){
+                codeMid.innerText += `${this.command("var","JS")} first_var = ${command[1]}`
+                codeMid.innerText += `\n${command[0]}("${command[2]}", first_var)\n`
+                
+            }
+            if(this.identyType(rawCommand) == "print_no_var"){
+                codeMid.innerText += `${command[0]}("${command[1]}")\n`
+                
+            }
+            if(this.identyType(rawCommand) == "if"){
+                let val_1 = command[1]
+                let val_2 = command[2]
+                let cond = command[3]
+                let inp = document.getElementById("cond")
+                let btn = document.getElementById("condButton");
+                btn.style.display= "block"
+                inp.style.display= "block"
+                btn.onclick = function()
+                {
+                    translateCallBack(inp.value, "JS")
+                    
+
+                }
+                codeMid.innerText += `${command[0]}(${val_1} ${cond} ${val_2}){\n   `
+                count(1)
+            }
+        }
+    }
+    translate(userCommand, lang){
+        if(lang == "JAVA")
+            this.structure(userCommand,this.identyCommand(userCommand,lang), lang)
+        if(lang == "PYTHON")
+            this.structure(userCommand,this.identyCommand(userCommand,lang), lang)
+        if(lang == "JS")
+            this.structure(userCommand,this.identyCommand(userCommand,lang), lang)
+    }
+    
+}
+var codeTranslate = new Translate();
+function translateCallBack(command, lang){
+   return codeTranslate.translate(command, lang)
+}   
+$(function(){
+    $('#command').autocomplete({
+        minLength: 2,
+		source: function (request, response) {
+			response($.map(AutoCompleteValue, function (obj, key) {
+				
+				var name = obj.syntax.toUpperCase();
+				
+				if (name.indexOf(request.term.toUpperCase()) != -1) {				
+					return {
+						label: obj.syntax,
+                        desc: obj.usage
+					}
+				} else {
+					return null;
+				}
+			}));			
+		}
+    }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+        return $( "<li></li>" )
+            .data( "item.autocomplete", item )
+            .append( "<a>" + item.label + "<p>"+ item.desc + "</p></a>" )
+            .appendTo( ul );
+            
+    };
+
+    $('#cond').autocomplete({
+        minLength: 2,
+		source: function (request, response) {
+			response($.map(AutoCompleteValue, function (obj, key) {
+				
+				var name = obj.syntax.toUpperCase();
+				
+				if (name.indexOf(request.term.toUpperCase()) != -1) {				
+					return {
+						label: obj.syntax,
+                        desc: obj.usage
+					}
+				} else {
+					return null;
+				}
+			}));			
+		}
+    }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+        return $( "<li></li>" )
+            .data( "item.autocomplete", item )
+            .append( "<a>" + item.label + "<p>"+ item.desc + "</p></a>" )
+            .appendTo( ul );
+            
+    };
+});
+function translateCommand(){
+        cleanCode();
+        let LangSelect = document.querySelector('input[name="lang"]:checked').value;
+        let commandFromUser = document.getElementById("command").value;
+        switch(LangSelect){
+            case 'Python':
+                codeTranslate.translate(commandFromUser, "PYTHON");
+                break;
+            case 'JAVA':
+                //JAVA_args.returnJAVA(commandFromUser);
+                codeTranslate.translate(commandFromUser, "JAVA");
+                break;
+            case 'JS':
+                codeTranslate.translate(commandFromUser, "JS")
+                break;
+        
+        }
+}
+
+const cleanCode = () =>{
+    document.getElementById("start").innerText = ``
+    document.getElementById("mid").innerText = ``
+    document.getElementById("end").innerText = ``
+}
